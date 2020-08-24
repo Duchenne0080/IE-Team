fef06e4b903bca4e856f9ed2fcb7c8ef4b5bdd67
Duchenne0080 <64134744+Duchenne0080@users.noreply.github.com>
2020-08-24 15:08:59 +1000

    Add files via upload

--- server_setup.sh
+++ server_setup.sh
@@ -3,36 +3,36 @@
 #E-mail: zxcvbnm_33961@qq.com
 
 echo "server set up......"
-echo `sudo -s`
-echo `apt-get update`
-echo `apt-get upgrade`
+sudo -s
+apt-get update
+apt-get upgrade
 
 
 echo "Install mysql server"
-echo `apt-get install mysql-server`
+apt-get install mysql-server
 echo "Mysql Server installed"
 
 echo "Install apache2 server"
-echo `apt-get install apache2`
+apt-get install apache2
 echo "Apache server installed"
 
 echo "Install PHP7.2 and other package set"
-echo `apt-get install php7.2`
-echo `apt-get install php7.2-fpm`
-echo `apt-get install php7.2-mysql`
-echo `apt-get install php libapache2-mod-php`
-echo `service apache2 restart`
+apt-get install php7.2
+apt-get install php7.2-fpm
+apt-get install php7.2-mysql
+apt-get install php libapache2-mod-php
+service apache2 restart
 
 echo "Install Phpmyadmin"
-echo `apt-get install phpmyadmin php7.2-mbstring php7.2-gettext`
-echo `ln -s /usr/share/phpmyadmin/ /var/www/html/`
-echo `systemctl restart apache2`
+apt-get install phpmyadmin php7.2-mbstring php7.2-gettext
+ln -s /usr/share/phpmyadmin/ /var/www/html/
+systemctl restart apache2
 
 echo "Downloading Wordpress online"
-echo `wget https://wordpress.org/latest.zip`
-echo `apt-get install unzip`
-echo `unzip latest.zip`
-echo `mv wordpress /var/www/html/`
+wget https://wordpress.org/latest.zip
+apt-get install unzip
+unzip latest.zip
+mv wordpress /var/www/html/
 
 echo "Your wordpress url: your-ip/wordpress"
 echo "e.g. 1.1.1.1/wordpress"
